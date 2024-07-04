0ee4255e8e196c93cf0b338d7e618f997ea56bad
Shoaib009 <kiet.shoaib@gmail.com>
2024-07-05 00:50:08 +0500

    inheritance and ploymorphism example done in todays code

---
+++ Models/TechCompany.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondProject.Models
+{
+    internal class TechCompany : Company
+    {
+        public string MainProduct { get; set; }
+
+        public int Budget { get; set; }
+        //public override int CompanyRavenue()
+        //{
+        //    return NumberOfEmployee * 100 / 2;
+
+        //}
+
+    }
+}
