b79e83f0630e84528223a7ea417b8c44cc1cad25
Shoaib009 <kiet.shoaib@gmail.com>
2024-07-10 01:13:14 +0500

    calculator example with complete checks

---
+++ Models/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondProject.Models
+{
+    public class Calculator
+    {
+        public int add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+        public int minus(int firstNumber, int secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+        public int multiply(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+        public int divide(int firstNumber, int secondNumber)
+        {
+            return firstNumber / secondNumber;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,21 +1,62 @@
 ﻿using SecondProject.Models;
 
-Company company = new Company();
-Company company2 = new Company("Tapal","ABC 123",22);
-TechCompany techCompany = new TechCompany();
-techCompany.MainProduct = "abc";
-techCompany.Budget = 500000;
-techCompany.CompanyName = "Gerrys Visa";
-//techCompany.NumberOfEmployee = 25;
-techCompany.CompanyAddress = "abc 123";
-//company.CompanyName = "Gerrys Visa";
-//Console.WriteLine(company.CompanyName);
-//company.Work();
-//company.SetCompanyName(3);
-company.Setnumberofemployee(11);
-Console.WriteLine(company.CompanyRavenue());
-//Console.WriteLine(techCompany.CompanyRavenue());
-//company.CompanyInfo();
-//techCompany.CompanyInfo();
-//company2.CompanyInfo();
-Console.ReadLine();
+Calculator calculator = new Calculator();
+string firstInput;
+string secondInput;
+string thirdInput;
+int firstNumber;
+int secondNumber;
+int seletecOperation;
+bool isNumber;
+do
+{
+    Console.WriteLine("enter first number");
+    firstInput = Console.ReadLine();
+    isNumber = int.TryParse(firstInput, out firstNumber);
+} while (isNumber == false);
+do
+{
+    Console.WriteLine("enter Second number");
+    secondInput = Console.ReadLine();
+    isNumber = int.TryParse(secondInput, out secondNumber);
+} while (isNumber == false);
+do
+{
+    Console.WriteLine("for addition press 1, for subtraction press 2, for multiplication press 3, for division press 4");
+    thirdInput = Console.ReadLine();
+    isNumber = int.TryParse(thirdInput, out seletecOperation);
+    if (isNumber)
+    {
+        if (0 < seletecOperation && seletecOperation < 5)
+        {
+            switch (seletecOperation)
+            {
+                case 1:
+                    Console.WriteLine(calculator.add(firstNumber, secondNumber));
+                    break;
+                case 2:
+                    Console.WriteLine(calculator.minus(firstNumber, secondNumber));
+                    break;
+                case 3:
+                    Console.WriteLine(calculator.multiply(firstNumber, secondNumber));
+                    break;
+                case 4:
+                    Console.WriteLine(calculator.divide(firstNumber, secondNumber));
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("please select the number between 1 to 4.");
+        }
+
+    }
+    else
+    {
+        Console.WriteLine("please select the correct number to perform that operation.");
+    }
+} while (isNumber == false || seletecOperation < 1 || seletecOperation > 4);
+//first the input is number.
+//second the number is in between 1 to 4.
+//input is number and input is between 1 to 4.
+//input is not a number or input is less then 1 or great then 4 . 
