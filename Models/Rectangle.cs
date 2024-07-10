a036eac9b44cd39f856514503ed41cfa03b54bee
Shoaib009 <kiet.shoaib@gmail.com>
2024-07-10 23:20:24 +0500

    created a program which will tell the user about area of rectangle

---
+++ Models/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondProject.Models
+{
+    public class Rectangle
+    {
+        public int AreaOfRectangle(int length, int width)
+        {
+            return length * width;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,21 +1,21 @@
 ﻿using SecondProject.Models;
-
-Company company = new Company();
-Company company2 = new Company("Tapal","ABC 123",22);
-TechCompany techCompany = new TechCompany();
-techCompany.MainProduct = "abc";
-techCompany.Budget = 500000;
-techCompany.CompanyName = "Gerrys Visa";
-//techCompany.NumberOfEmployee = 25;
-techCompany.CompanyAddress = "abc 123";
-//company.CompanyName = "Gerrys Visa";
-//Console.WriteLine(company.CompanyName);
-//company.Work();
-//company.SetCompanyName(3);
-company.Setnumberofemployee(11);
-Console.WriteLine(company.CompanyRavenue());
-//Console.WriteLine(techCompany.CompanyRavenue());
-//company.CompanyInfo();
-//techCompany.CompanyInfo();
-//company2.CompanyInfo();
+Rectangle rectangle = new Rectangle();
+string firstNumber;
+string secondNumber;
+int firstValue;
+int secondValue;
+bool isNumber;
+do
+{
+    Console.WriteLine("enter first number");
+    firstNumber = Console.ReadLine();
+    isNumber = int.TryParse(firstNumber, out firstValue);
+} while (isNumber == false);
+do
+{
+    Console.WriteLine("enter first number");
+    secondNumber = Console.ReadLine();
+    isNumber = int.TryParse(secondNumber, out secondValue);
+} while (isNumber == false);
+Console.WriteLine( "Area of Rectangle: " + rectangle.AreaOfRectangle(firstValue, secondValue));
 Console.ReadLine();
