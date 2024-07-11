7c21bced2c7ef28cdd57f4e4d206c2fe620c6ff9
Shoaib009 <kiet.shoaib@gmail.com>
2024-07-11 23:41:19 +0500

    User input message change.

--- Program.cs
+++ Program.cs
@@ -7,15 +7,15 @@ int secondValue;
 bool isNumber;
 do
 {
-    Console.WriteLine("enter first number");
+    Console.WriteLine("To calculate the area of rectangle please enter the length of rectangle.");
     firstNumber = Console.ReadLine();
     isNumber = int.TryParse(firstNumber, out firstValue);
 } while (isNumber == false);
 do
 {
-    Console.WriteLine("enter first number");
+    Console.WriteLine("To calculate the area of rectangle please enter the width of the rectangle.");
     secondNumber = Console.ReadLine();
     isNumber = int.TryParse(secondNumber, out secondValue);
 } while (isNumber == false);
-Console.WriteLine( "Area of Rectangle: " + rectangle.AreaOfRectangle(firstValue, secondValue));
+Console.WriteLine("Area of Rectangle: " + rectangle.AreaOfRectangle(firstValue, secondValue));
 Console.ReadLine();
