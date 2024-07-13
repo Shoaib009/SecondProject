b29a2c6eff5d9175e09bcc71f321dec92a909fd4
Shoaib009 <kiet.shoaib@gmail.com>
2024-07-13 23:09:36 +0500

    temperature convert from celcius to farenheit or farenheit to celcius

---
+++ Models/TemperatureConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondLearningCShrapProject
+{
+    public class TemperatureConverter
+    {
+        public float FarhenheitToCelcius(float temperatureValue)
+        {
+            var temp = 5 / 9f;
+            var temp1 = temperatureValue - 32f;
+            temp = temp1 * temp;
+            return temp;
+        }
+        public float CelciusToFarhenheit(float temperatureValue)
+        {
+            float temp = 9 / 5f;
+            temp = temperatureValue * temp;
+            temp = temp + 32;
+            return temp;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,21 +1,43 @@
-﻿using SecondProject.Models;
+﻿using SecondLearningCShrapProject;
 
-Company company = new Company();
-Company company2 = new Company("Tapal","ABC 123",22);
-TechCompany techCompany = new TechCompany();
-techCompany.MainProduct = "abc";
-techCompany.Budget = 500000;
-techCompany.CompanyName = "Gerrys Visa";
-//techCompany.NumberOfEmployee = 25;
-techCompany.CompanyAddress = "abc 123";
-//company.CompanyName = "Gerrys Visa";
-//Console.WriteLine(company.CompanyName);
-//company.Work();
-//company.SetCompanyName(3);
-company.Setnumberofemployee(11);
-Console.WriteLine(company.CompanyRavenue());
-//Console.WriteLine(techCompany.CompanyRavenue());
-//company.CompanyInfo();
-//techCompany.CompanyInfo();
-//company2.CompanyInfo();
-Console.ReadLine();
+TemperatureConverter temperatureConverter = new TemperatureConverter();
+string temperature;
+string userSelection;
+float temperatureValue;
+int userValue;
+bool IsNumber;
+do
+{
+    Console.WriteLine("Fahenheit to Celcius Convertion Press 1: \nCelcius to Fahenheit convertion Press 2: ");
+    userSelection = Console.ReadLine();
+    IsNumber = int.TryParse(userSelection, out userValue);
+    if (!IsNumber || (IsNumber && (userValue < 1 || userValue > 2)))
+    {
+        Console.WriteLine("Please select the right value.");
+    }
+} while (!IsNumber || (IsNumber && (userValue < 1 || userValue > 2)));// all conditions are true then this break out of this loop 
+
+do
+{
+    if (userValue == 1)
+        Console.WriteLine("Please enter Fahenheit value:");
+    if (userValue == 2)
+        Console.WriteLine("Please enter Celcius value:");
+
+    temperature = Console.ReadLine();
+    IsNumber = float.TryParse(temperature, out temperatureValue);
+} while (IsNumber == false);
+
+switch (userValue)
+{
+    case 1:
+        Console.WriteLine($"Temperature Value in Celcius was {temperatureValue}, and Fahenheit value is " +
+                          $"{temperatureConverter.CelciusToFarhenheit(temperatureValue)}");
+        break;
+    case 2:
+        Console.WriteLine($"Temperature Value in Fahenheit was {temperatureValue}, and Celcius value is " +
+                          $"{temperatureConverter.FarhenheitToCelcius(temperatureValue)}");
+        break;
+    default:
+        break;
+}
