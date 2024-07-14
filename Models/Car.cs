7c3f9c7324259836b289e49b95ef56f838f7eb62
Shoaib009 <kiet.shoaib@gmail.com>
2024-07-14 23:01:44 +0500

    created car class and inherit this class with honda class.

--- Models/Car.cs
+++ Models/Car.cs
@@ -8,5 +8,17 @@ namespace SecondProject.Models
 {
     internal class Car
     {
+        public string companyName { get; set; }
+        public int model { get; set; }
+        public string carColor { get; set; }
+        public void CarStart() { Console.WriteLine("car is start now... "); }
+        public void CarStop() { Console.WriteLine("car is stoped now."); }
+    }
+    internal class Honda : Car
+    {
+        public void NewModel()
+        {
+            Console.WriteLine("this is Honda new model 2024.");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using SecondProject.Models;
+
+Car car = new Car();
+Honda honda = new Honda();
+car.companyName = "Merdedes";
+car.model = 2024;
+car.carColor = "Silver black";
+car.CarStart();
+car.CarStop();
+honda.companyName = "Honda";
+honda.carColor = "Black";
+honda.model = 2023;
+honda.NewModel();
