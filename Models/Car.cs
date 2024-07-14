0cf224f494e948eb66bfa36df222a8b6989862a1
Shoaib009 <kiet.shoaib@gmail.com>
2024-07-15 00:52:20 +0500

    inheritance base polymorphism (method overriding).

--- Models/Car.cs
+++ Models/Car.cs
@@ -13,6 +13,8 @@ namespace SecondProject.Models
         public string carColor { get; set; }
         public void CarStart() { Console.WriteLine("car is start now... "); }
         public void CarStop() { Console.WriteLine("car is stoped now."); }
+
+        public virtual void CarFeulType() { Console.WriteLine("car use petrol as a feul"); }
     }
     internal class Honda : Car
     {
@@ -20,5 +22,17 @@ namespace SecondProject.Models
         {
             Console.WriteLine("this is Honda new model 2024.");
         }
+        public override void CarFeulType()
+        {
+            Console.WriteLine("car use desel as a feul");
+        }
+    }
+    internal class Toyota : Car
+    {
+        public void CompanyName()
+        {
+            Console.WriteLine("this is Toyota company.");
+        }
+        public override void CarFeulType() { Console.WriteLine("car use battery as a feul"); }
     }
 }
