f3de3804b10ecd0678d6c5569d184ba8b45b883f
Shoaib009 <kiet.shoaib@gmail.com>
2024-07-16 14:25:07 +0500

    Bank account class with properties and methods.

---
+++ Models/Accounts.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace SecondProject.Models
+{
+    internal class Accounts
+    {
+        int availableCash = 0;
+        int _accountHolderId;
+        string _accountHolderName;
+        string _accountName;
+
+        public Accounts(int id, string accountHolderName, string accountName)
+        {
+            _accountHolderId = id;
+            _accountHolderName = accountHolderName;
+            _accountName = accountName;
+        }
+        public int DepositeCash(int amount) { 
+            availableCash += amount;
+            return availableCash;
+        }
+        public int WithdrawalCash(int amount)
+        {
+            availableCash -= amount;
+            return availableCash;
+        }
+        public int BalanceInquiry() { return availableCash; }
+    }
+}
