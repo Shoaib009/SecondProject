642df770ac9aa9cbc6408a49f64c43dc4574b27e
Shoaib009 <kiet.shoaib@gmail.com>
2024-07-16 13:13:30 +0500

    bad logic.

--- Models/Accounts.cs
+++ Models/Accounts.cs
@@ -2,11 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace SecondProject.Models
 {
     internal class Accounts
     {
+        const int cashAvailable = 0;
+        public string accountHolderName { get; set; }
+        public int accountHolderId { get; set; }
+
+        public string accountTitle { get; set; }
+
+        public int CashDeposite(int amount) { return cashAvailable + amount; }
+        public int CashWithdrawal(int amount) { return cashAvailable - amount; }
+        public int CheckBalance() { return cashAvailable; }
+
+
     }
 }
