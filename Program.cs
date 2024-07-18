004d6bf4232a30c82540e693ff8b9f2b8ff1c8a5
Shoaib009 <kiet.shoaib@gmail.com>
2024-07-19 01:35:22 +0500

    adding and removing elements in the list also showing the output on the screen.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+List<int> number = new List<int> { 1, 2, 3 };
+number.Add(123);
+foreach (int i in number)
+{
+    Console.WriteLine(i);
+}
+number.RemoveAt(3);
+number.Add(11);
+Console.WriteLine("\n");
+for (int i = 0; i < number.Count; i++)
+{
+    Console.WriteLine(number[i]);
+}
