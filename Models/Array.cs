f805eab930d2ca0b6d63c6ad3cc2de0f63f7f534
Shoaib009 <kiet.shoaib@gmail.com>
2024-07-18 23:39:40 +0500

    Arrays.

---
+++ Models/Array.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondProject.Models
+{
+    internal class Array
+    {
+        int[] numbers = new int[10];
+    }
+}
