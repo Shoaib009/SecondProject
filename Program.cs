4b676312767201a46f538bdec613c82974abbdfa
Shoaib009 <kiet.shoaib@gmail.com>
2024-07-22 23:15:43 +0500

    dictionary example with user input.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+﻿
+using SecondProject.Model;
+using System;
+using System.Collections.Generic;
+
+
+Dictionary<int, string> keyValuePairs = new Dictionary<int, string>();
+do
+{
+    Teacher teacher = new Teacher();
+    Console.WriteLine("enter teacher id: ");
+    teacher.teacherId = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("enter teacher name: ");
+    teacher.teacherName = Console.ReadLine();
+    keyValuePairs.Add(teacher.teacherId, teacher.teacherName);
+} while (keyValuePairs.Count <= 4);
+keyValuePairs.Remove(0);
+foreach (var kvp in keyValuePairs)
+{
+    Console.WriteLine("Teacher ID is {0} \nTeacher Name is {1}",kvp.Key, kvp.Value);
+}
+
+
