27a344ae933e0ed7e5e96dcfccb6e93872a7f91a
Shoaib009 <kiet.shoaib@gmail.com>
2024-07-25 23:44:25 +0500

    we make a program which check the exception divide by zero.

---
+++ Model/Division.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace SecondProject.Model
+{
+    internal class Division
+    {
+
+        public int numinator { get; set; }
+        public int denominator { get; set; }
+
+
+        public int DividTheseNumbers(int numerator, int denominator)
+        {
+            try
+            {
+            return DivisionFunc(numerator, denominator);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public int DivisionFunc(int value1, int value2)
+        {
+           
+            try
+            {
+                return value1 / value2;
+            }
+            catch (DivideByZeroException ex)
+            {
+                throw ex;
+                //Console.WriteLine(ex.Message);
+            }
+
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,21 +3,56 @@ using SecondProject.Model;
 using System;
 using System.Collections.Generic;
 
+    throw new Exception("Student object is null. ");
+try
+{
+    Division division = new Division();
+    int total;
+    do
+    {
+        Console.WriteLine("enter the numinator value:");
+        division.numinator = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("enter denominator value: ");
+        division.denominator = Convert.ToInt32(Console.ReadLine());
+     // division.DividTheseNumbers(division.numinator, division.denominator);
+       // total = division.DividTheseNumbers(division.numinator, division.denominator);
 
-Dictionary<int, string> keyValuePairs = new Dictionary<int, string>();
-do
+    } while (division.denominator > 0);
+   // Console.WriteLine(total);
+}
+catch (DivideByZeroException)
 {
-    Teacher teacher = new Teacher();
-    Console.WriteLine("enter teacher id: ");
-    teacher.teacherId = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("enter teacher name: ");
-    teacher.teacherName = Console.ReadLine();
-    keyValuePairs.Add(teacher.teacherId, teacher.teacherName);
-} while (keyValuePairs.Count <= 4);
-keyValuePairs.Remove(0);
-foreach (var kvp in keyValuePairs)
-{
-    Console.WriteLine("Teacher ID is {0} \nTeacher Name is {1}",kvp.Key, kvp.Value);
+
+    Console.WriteLine("abc");
 }
 
+//TextWriter tw;
+
+//try
+//{
+//    string path = @"D:\code\Example.txt";
+//    if (!File.Exists(path))
+//    {
+//        File.Create(path);
+//        tw = new StreamWriter(path);
+//        tw.WriteLine("The very first line!");
+//        tw.Close();
+//    }
+//    else if (File.Exists(path))
+//    {
+//        tw = new StreamWriter(path, true);
+//        tw.WriteLine("The next line!");
+//        tw.Close();
+//    }
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine("Error occurred: {0}", ex.Message);
+//}
+//finally
+//{
+//    // clean up file object here;
+//    tw = null;
+
+//}
 
