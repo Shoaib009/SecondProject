b04f021e833560809a96728ff477f730ccce2f7d
Shoaib009 <kiet.shoaib@gmail.com>
2024-07-29 23:28:32 +0500

    no changes.

--- Program.cs
+++ Program.cs
@@ -5,19 +5,20 @@ using System.Collections.Generic;
 
 
 Dictionary<int, string> keyValuePairs = new Dictionary<int, string>();
+Teacher teacher = new Teacher();
 do
 {
-    Teacher teacher = new Teacher();
     Console.WriteLine("enter teacher id: ");
     teacher.teacherId = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine("enter teacher name: ");
     teacher.teacherName = Console.ReadLine();
     keyValuePairs.Add(teacher.teacherId, teacher.teacherName);
-} while (keyValuePairs.Count <= 4);
-keyValuePairs.Remove(0);
+} while (keyValuePairs.Count <= 2);
+
+
 foreach (var kvp in keyValuePairs)
 {
-    Console.WriteLine("Teacher ID is {0} \nTeacher Name is {1}",kvp.Key, kvp.Value);
+    Console.WriteLine("Teacher ID is {0} \nTeacher Name is {1}", kvp.Key, kvp.Value);
 }
 
 
