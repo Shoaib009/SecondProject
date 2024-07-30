0304c61741d79fa7dec6e52fe38dc29d9ecfd257
Shoaib009 <kiet.shoaib@gmail.com>
2024-07-31 01:22:14 +0500

    checking key and value through user input and removing a value.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@ using System.Collections.Generic;
 
 Dictionary<int, string> keyValuePairs = new Dictionary<int, string>();
 Teacher teacher = new Teacher();
+int dictKey;
+string dicValue;
+int myKey = 0;
+int temp = 0;
 do
 {
     Console.WriteLine("enter teacher id: ");
@@ -21,4 +25,38 @@ foreach (var kvp in keyValuePairs)
     Console.WriteLine("Teacher ID is {0} \nTeacher Name is {1}", kvp.Key, kvp.Value);
 }
 
+do
+{
+    Console.WriteLine("enter the key to check its value: ");
+    dictKey = Convert.ToInt32(Console.ReadLine());
+    if (keyValuePairs.ContainsKey(dictKey))
+    {
+        Console.WriteLine("The value of entered key is {0} and its key is {1}", keyValuePairs[dictKey], dictKey);
+    }
+    else
+    {
+        Console.WriteLine("your entered key is not found in the list please try again");
+    }
+} while (keyValuePairs.ContainsKey(dictKey) == false);
 
+do
+{
+    Console.WriteLine("enter the value to check its if its in the list: ");
+    dicValue = Console.ReadLine();
+    if (keyValuePairs.ContainsValue(dicValue))
+    {
+        myKey = keyValuePairs.FirstOrDefault(x => x.Value == dicValue).Key;
+        Console.WriteLine("The value of entered key is {0} and its key is {1}", keyValuePairs[myKey], myKey);
+        keyValuePairs.Remove(myKey);
+        temp = 1;
+    }
+    else
+    {
+        Console.WriteLine("your entered key is not found in the list please try again");
+    }
+} while (temp != 1);
+
+foreach (var kvp in keyValuePairs)
+{
+    Console.WriteLine("Teacher ID is {0} \nTeacher Name is {1}", kvp.Key, kvp.Value);
+}
